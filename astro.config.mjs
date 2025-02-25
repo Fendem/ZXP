aec46edded179fcae205a2dcd0e0a2b4738cbfe2
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-26 00:50:56 +0100

    Making it server site!

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({ mode: "serverless" }),
   output: "server",
   vite: {
     plugins: [tailwindcss()]
