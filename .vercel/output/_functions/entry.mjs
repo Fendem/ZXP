8d99e5f16d168ed5281c74a8f686dd4f484fd0b5
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-26 01:50:16 +0100

    vercel

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,27 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CYcLMXeL.mjs';
+import { manifest } from './manifest_QI3lQPK3.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "c1655622-e7fa-4e77-b1f2-6c28fdc64847",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
