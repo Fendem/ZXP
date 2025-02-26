6e48c1b958f1450895f451f5b8333126e8c47163
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-26 01:37:52 +0100

    testing

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Ces9jwNA.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,5 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  adapter: vercel({
-    mode: 'serverless', // Enable serverless functions on Vercel
-  }),
-  output: 'server', // Explicitly set Astro to server-side rendering (SSR)
+  adapter: vercel(),
 });
