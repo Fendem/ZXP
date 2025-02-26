004a6ca00a430a3ee98def7564409d2626a10668
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-26 01:56:29 +0100

    vercel

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CYcLMXeL.mjs';
-import { manifest } from './manifest_K_oyX_x4.mjs';
+import { manifest } from './manifest_BXOOviSP.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "470f062e-28ce-4c86-bcde-bc63caeca3f3",
+    "middlewareSecret": "ed6e8c72-eaf7-4938-82b7-300ff4a531bd",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
